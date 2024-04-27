345abd93e4b5d74e7451ca4abb12a988d2ff5284
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-04-27 22:31:35 +0600

    Add Variable Environment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,8 +12,7 @@ app.use(express.json());
 // Art&Craft
 // ueKtqrPA3zmIPW0O
 
-const uri =
-  "mongodb+srv://Art_Craft:ueKtqrPA3zmIPW0O@cluster0.dbn21dt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dbn21dt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,6 +31,11 @@ async function run() {
     const craftCollection = client.db("craftDB").collection("craft");
 
     app.get("/crafts", async (req, res) => {
+      const cursor = craftCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/craftss", async (req, res) => {
       const cursor = craftCollection.find();
       const result = await cursor.toArray();
       res.send(result);
