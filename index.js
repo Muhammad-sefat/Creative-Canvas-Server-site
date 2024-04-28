53d362ef499111866a8a6c5443707e334a2cbddb
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-04-28 11:07:00 +0600

    Craft Delete from database

--- index.js
+++ index.js
@@ -76,6 +76,13 @@ async function run() {
       const result = await craftCollection.insertOne(newCraft);
       res.send(result);
     });
+
+    app.delete("/crafts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
